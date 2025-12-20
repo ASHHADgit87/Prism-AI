2be21cb1dbe3d80e75b9eaa8050a75b81b59b898
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2025-12-20 14:12:05 +0500

    Added two division one for message container and other for input area and added tailwind css to message div

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -11,6 +11,14 @@ const Sidebar = ({isMenuOpen,project,setProject,isGenerating,setIsGenerating} : 
   return (
     <div className = {`h-full sm:max-w-sm rounded-xl bg-gray-900 border-gray-800 transition-all ${isMenuOpen ? 'max-sm:w-0 overflow-hidden' : 'w-full'}`}>
         <div className='flex flex-col h-full'>
+            {/* Message Container*/}
+            <div className='flex-1 overflow-y-auto no-scrollbar px-3 flex flex-col gap-4'>
+                {[]}
+            </div>
+            {/* Input Area*/}
+            <form>
+
+            </form>
         </div>   
     </div>
   )

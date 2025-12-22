2dcf1a28fedde9f3f02eb07da97c4ff7c3b79e53
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2025-12-22 18:55:06 +0500

    added c;lasses for preview of code and also added for each phone,tablet and desktop

--- client/src/components/ProjectPreview.tsx
+++ client/src/components/ProjectPreview.tsx
@@ -14,6 +14,11 @@ export interface ProjectPreviewRef {
 
 const ProjectPreview = forwardRef<ProjectPreviewRef, ProjectPreviewProps>(({project,isGenerating,device = 'desktop',showEditorPanel = true},ref) => {
     const iframeRef = useRef<HTMLIFrameElement>(null);
+    const resolutions = {
+        phone : 'w-[412px]',
+        tablet : 'w-[768px]',
+        desktop : 'w-full'
+    }
     const injectPreview = (html: string) => {
         if(!html) return '';
         if(!showEditorPanel) return html;
@@ -27,7 +32,7 @@ const ProjectPreview = forwardRef<ProjectPreviewRef, ProjectPreviewProps>(({proj
     <div className='relative h-full bg-gray-900 flex-1 rounded-xl overflow-hidden max-sm:ml-2'>
         {project.current_code?(
             <>
-                    <iframe ref={iframeRef} className='w-full h-full' srcDoc={injectPreview(project.current_code)}/>
+                    <iframe ref={iframeRef}  srcDoc={injectPreview(project.current_code)}className={`h-full max-sm:w-full ${resolutions[device]} max-auto transition-all`} />
             </>
         )
         :

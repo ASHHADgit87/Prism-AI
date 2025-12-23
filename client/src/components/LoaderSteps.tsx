67a610015e6d4cee01f98d9aec6dbd9b3bf35dfc
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2025-12-23 16:07:16 +0500

    Added four indented divisions and also added mighty tailwind css to three of them

--- client/src/components/LoaderSteps.tsx
+++ client/src/components/LoaderSteps.tsx
@@ -9,8 +9,12 @@ const steps = [
 const STEP_DURATION = 45000
 const LoaderSteps = () => {
   return (
-    <div>
-
+    <div className='w-full h-full flex flex-col items-center justify-center bg-gray-950 relative overflow-hidden text-white'>
+     <div className='absolute inset-0 bg-linear-to-br from-blue-500/10 via-purple-500/10 to-fuchsia-500/10 blur-3xl animate-pulse'>
+       <div className='relative z-10 w-32 h-32 flex items-center justify-center'>
+          <div/>
+       </div>
+     </div>
     </div>
   )
 }

88d8ff2c00bdd0ef9d34fa17a2ac07e584b47d8a
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2025-12-24 15:03:02 +0500

    Completed our view page

--- client/src/pages/View.tsx
+++ client/src/pages/View.tsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { dummyProjects } from '../assets/assets';
+import { Loader2Icon } from 'lucide-react';
+import ProjectPreview from '../components/ProjectPreview';
+import type { Project } from '../types';
 
 const View = () => {
   const {projectId} = useParams();
@@ -15,8 +18,16 @@ const View = () => {
       }
     },2000)
   }
+  useEffect(() => {
+    fetchCode();
+  },[])
+  if(loading){
+    return(<div className='flex items-center justify-center h-screen'><Loader2Icon className='size-7 animate-spin text-indigo-200'/></div>)
+  }
   return (
-    <div><h1>View</h1></div>
+    <div>
+      {code && <ProjectPreview project={{current_code:code} as Project}isGenerating={false} showEditorPanel={false}/> }
+    </div>
   )
 }
 

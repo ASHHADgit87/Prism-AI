05004716073f30e05a0433ded915a27d7f639294
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2025-12-24 14:56:28 +0500

    Added css styling for animate fade in classes in index.css file and also added it in editor panel file and startring wokring on view page

--- client/src/pages/View.tsx
+++ client/src/pages/View.tsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { dummyProjects } from '../assets/assets';
 
 const View = () => {
+  const {projectId} = useParams();
+  const [code,setCode] = useState('');
+  const [loading,setLoading] = useState(true);
+  const fetchCode = async() =>{
+    const code = dummyProjects.find(project => project.id === projectId )?.current_code;
+    setTimeout(() => {
+      if(code){
+        setCode(code);
+        setLoading(false);
+      }
+    },2000)
+  }
   return (
     <div><h1>View</h1></div>
   )

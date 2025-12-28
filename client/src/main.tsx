4c294ae5cce916c39f39dca87b7718273284e0b1
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2025-12-28 13:28:49 +0500

    now Done with setup and routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,9 @@ import App from './App.tsx'
 import {BrowserRouter} from 'react-router-dom'
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+  <Providers>
+     <App />
+  </Providers>
+    
   </BrowserRouter>
 )

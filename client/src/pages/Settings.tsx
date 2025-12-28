a2655354fbf86bfa065f893fd712e00851f5dc66
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2025-12-28 22:33:08 +0500

    Now delte  and change password working fine

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -17,7 +17,7 @@ const Settings = () => {
          <div className="w-full">
             <DeleteAccountCard classNames={{
                  base: 'bg-black/10 ring ring-indigo-950 max-w-xl mx-auto',
-            footer : 'bg-black/10 ring ring-indigo-950'
+            
             }} />
         </div>
     </div>

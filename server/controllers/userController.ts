c8928f0d68a7369f33eba8b4ce26e411e000cc34
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2025-12-30 18:58:50 +0500

    Created a new file in new folder user controller and a function to get user credits

---
+++ server/controllers/userController.ts
@@ -0,0 +1,8 @@
+import {Request,Response} from "express";
+export const getUserCredits = async (req:Request, res:Response) => {
+    try {
+        const userId = req.userId;
+    } catch (error) {
+        
+    }
+}

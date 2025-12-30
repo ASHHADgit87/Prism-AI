84c702416b11545d89c6d2e6064d965a863c1e5b
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2025-12-30 21:03:09 +0500

    now working on user controller file and created more logic in get func

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,8 +1,18 @@
 import {Request,Response} from "express";
+import prisma from "../lib/prisma.js";
 export const getUserCredits = async (req:Request, res:Response) => {
     try {
         const userId = req.userId;
-    } catch (error) {
-        
+        if(!userId){
+            return res.status(401).json({message:"Unauthorized User"});  
+        }
+        const user = await prisma.user.findUnique({
+            where:{id: userId},
+            
+        })
+        res.json({credits: user?.credits});
+    } catch (error: any) {
+        console.log(error);
+        res.status(401).json({message: error.code || error.message});
     }
 }

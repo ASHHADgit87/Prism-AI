589d15daa7e8f32c2108bbc64d5754c1ce611256
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-01 22:18:49 +0500

    Now Adding new function to bbackend logic the createing project

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -16,3 +16,23 @@ export const getUserCredits = async (req:Request, res:Response) => {
         res.status(500).json({message: error.code || error.message});
     }
 }
+export const createUserProject = async (req:Request, res:Response) => {
+    const userId = req.userId;
+    try {
+        const {initial_prompt} = req.body;
+        if(!userId){
+            return res.status(401).json({message:"Unauthorized User"});  
+        }
+        const user = await prisma.user.findUnique({
+            where:{id: userId},
+            
+        })
+        if(user && user.credits < 5){
+            
+        }
+        res.json({credits: user?.credits});
+    } catch (error: any) {
+        console.log(error);
+        res.status(500).json({message: error.code || error.message});
+    }
+}

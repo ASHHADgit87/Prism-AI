c7623a611c59133165c04716f61e0a9af63041ed
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-03 22:22:41 +0500

    Mounted userrouter in main file

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import { toNodeHandler } from 'better-auth/node';
 import { auth } from './lib/auth.js';
+import userRouter from './routes/userRoutes.js';
 const app = express();
 
 const port = 3000;
@@ -18,7 +19,7 @@ app.all('/api/auth/{*any}', toNodeHandler(auth));
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
-
+app.use('/api/user', userRouter)
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });

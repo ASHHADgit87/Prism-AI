9eadf0ce469019645ecc6537d86a881918ea4f70
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-05 21:54:11 +0500

    added project id

--- server/routes/projectRoutes.ts
+++ server/routes/projectRoutes.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import { protect } from '../middleware/auth.js';
 import { deleteproject, getProjectById, getProjectPreview, getPublishedProjects, makeRevisions, rollbackToVersion, saveProjectCode } from '../controllers/projectController.js';
 const projectRouter = express.Router();
-projectRouter.post('/revision/',protect, makeRevisions );
+projectRouter.post('/revision/:projectId',protect, makeRevisions );
 projectRouter.put('/save/:projectId',protect, saveProjectCode );
 projectRouter.get('/rollback/:projectId/:versionId',protect, rollbackToVersion );
 projectRouter.delete('/:projectId',protect, deleteproject);

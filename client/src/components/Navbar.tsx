781a0ce42a11be7815d6166159655e96c7a21193
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-07 14:41:35 +0500

    Developed a new async func which will make credit left for each user in navbar also added use effect and now

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets } from '../assets/assets';
 import { Link, useNavigate } from 'react-router-dom';
 import { authClient } from '@/lib/auth-client';
 import {UserButton} from '@daveyplate/better-auth-ui'
+import api from '@/configs/axios';
+import { toast } from 'sonner';
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = React.useState(false);
     const navigate = useNavigate();
+    const [credits, setCredits] = useState(0);
+    
     const {data: session} = authClient.useSession();
+    const getCredits = async () => {
+        try {
+            const {data} = await api.get('/api/user/credits');
+            setCredits(data.credits);
+        } catch (error: any) {
+            toast.error(error?.response?.data?.message || error.message);
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        if(session?.user){
+            getCredits();
+        }
+    },[session?.user])
     return (
         <>
         <nav className="sticky top-0 z-50 flex items-center justify-between w-full py-4 px-4 md:px-16 lg:px-24 xl:px-32 backdrop-blur border-b text-white border-[#00331a]">

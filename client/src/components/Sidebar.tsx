85a4481520e9726d3ca544674b310b7f3f6b684f
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-10 13:05:31 +0500

    Removed an unnecessary import

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import type { Message, Project, Version } from '../types'
 import { BotIcon, EyeIcon, Loader2Icon, SendIcon, UserIcon } from 'lucide-react'
 import { Link } from 'react-router-dom'
-import { set } from 'better-auth'
+
 import api from '@/configs/axios'
 import { toast } from 'sonner'
 interface SidebarProps {

2dd6e94d5b5d471d3eee30ccaea18a4099dfe78e
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-10 14:14:21 +0500

    Removed unnessacry imports from 3 files

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react'
+import  { useEffect, useRef, useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import type { Project } from '../types';
-import { ArrowBigDownDashIcon, EyeIcon, EyeOffIcon, FileMinus, FullscreenIcon, LaptopIcon, Loader2Icon, MessageSquareIcon, SaveIcon, SmartphoneIcon, TabletIcon, XIcon } from 'lucide-react';
+import { ArrowBigDownDashIcon, EyeIcon, EyeOffIcon, FullscreenIcon, LaptopIcon, Loader2Icon, MessageSquareIcon, SaveIcon, SmartphoneIcon, TabletIcon, XIcon } from 'lucide-react';
 
 import Sidebar from '../components/Sidebar';
 import ProjectPreview from '../components/ProjectPreview';

1748cff40c7d7ec4a92879592f6f161495c95674
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-02-10 17:58:41 +0500

    Added community logic

--- server/routes/projectRoutes.ts
+++ server/routes/projectRoutes.ts
@@ -20,6 +20,6 @@ projectRouter.get(
 );
 projectRouter.delete("/:projectId", protect, deleteproject);
 projectRouter.get("/preview/:projectId", protect, getProjectPreview);
-projectRouter.get("/published", protect, getPublishedProjects);
+projectRouter.get("/published",  getPublishedProjects);
 projectRouter.get("/published/:projectId", protect, getProjectById);
 export default projectRouter;

c553ce27ce47740502c4b99d4f675345bcf1175f
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-03-01 16:30:54 +0500

    MADE footer good

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,11 +1,14 @@
-
+import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  const yearText = currentYear === 2025 ? "2025" : `2025-${currentYear}`;
+
   return (
     <div className=" bg-[#0073b3] text-center py-6 text-white text-md font-semibold border  mt-auto">
       <p>
-        Copyright © 2025 Prism AI - Muhammad Ashhadullah Zaheer. All rights
-        reserved
+        Copyright © {yearText} Prism AI - Muhammad Ashhadullah Zaheer. All
+        rights reserved
       </p>
     </div>
   );
